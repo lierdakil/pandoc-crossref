3f78ae331d34e3ee2341348eef6447c147663e86
Nikolay Yakimov <root@livid.pp.ru>
2015-04-13 06:21:22 +0300

    Add rudimentary Travis tests

---
+++ test-pandoc-crossref.hs
@@ -0,0 +1,17 @@
+import Test.Hspec
+import Text.Pandoc
+import System.Process
+-- import Control.Exception (evaluate)
+
+main :: IO ()
+main = do
+  native <- read `fmap` readFile "demo.native" :: IO [Block]
+  parsed <- read `fmap` readProcess "pandoc" [
+                              "-F", "pandoc-crossref.hs"
+                            , "-i", "demo.md"
+                            , "-t", "native"]
+                            ""
+  hspec $
+    describe "pandoc-crossref.hs" $
+      it "formats demo.md as demo.native" $
+        parsed `shouldBe` native
