0789c12f3a53669548e16f3facdd89fcac32ed8a
Nikolay Yakimov <root@livid.pp.ru>
2015-04-13 04:27:14 +0300

    Attempt at fixing GHC 7.10 compatibility

--- pandoc-crossref.hs
+++ pandoc-crossref.hs
@@ -176,7 +176,9 @@ toString _ = Nothing
 
 applyTemplate :: [Inline] -> [Inline] -> [Inline] -> WS [Inline]
 applyTemplate i t tmpl = withTmplV internalVars $ replaceTemplate tmpl
-  where withTmplV f g = do
+  where
+        withTmplV :: (String -> Maybe MetaValue) -> WS [Inline] -> WS [Inline]
+        withTmplV f g = do
           modify $ \s -> s{stTmplV=f}
           res <- g
           modify $ \s -> s{stTmplV=const Nothing}
