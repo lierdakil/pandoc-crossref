bce928fffb4b540e9ffff53dc9f56558809061db
Nikolay Yakimov <root@livid.pp.ru>
2015-10-04 00:15:01 +0300

    Don't use TemplateHaskell
    It dies on Windows

--- src/Text/Pandoc/CrossRef/Util/Settings/Gen.hs
+++ src/Text/Pandoc/CrossRef/Util/Settings/Gen.hs
@@ -1,27 +1,61 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 module Text.Pandoc.CrossRef.Util.Settings.Gen where
 
 import Text.Pandoc.CrossRef.Util.Settings.Template
+import Text.Pandoc.Builder
 
-concat `fmap` mapM genSetting
-  [ "figureTitle"
-  , "tableTitle"
-  , "listingTitle"
-  , "titleDelim"
-  , "chapDelim"
-  , "rangeDelim"
-  , "figPrefix"
-  , "eqnPrefix"
-  , "tblPrefix"
-  , "lstPrefix"
-  , "secPrefix"
-  , "lofTitle"
-  , "lotTitle"
-  , "lolTitle"
-  , "figureTemplate"
-  , "tableTemplate"
-  , "listingTemplate"
-  , "crossrefYaml"
-  , "chaptersDepth"
-  ]
+figureTitle :: ToMetaValue a => a -> Meta
+figureTitle = template "figureTitle"
+
+tableTitle :: ToMetaValue a => a -> Meta
+tableTitle = template "tableTitle"
+
+listingTitle :: ToMetaValue a => a -> Meta
+listingTitle = template "listingTitle"
+
+titleDelim :: ToMetaValue a => a -> Meta
+titleDelim = template "titleDelim"
+
+chapDelim :: ToMetaValue a => a -> Meta
+chapDelim = template "chapDelim"
+
+rangeDelim :: ToMetaValue a => a -> Meta
+rangeDelim = template "rangeDelim"
+
+figPrefix :: ToMetaValue a => a -> Meta
+figPrefix = template "figPrefix"
+
+eqnPrefix :: ToMetaValue a => a -> Meta
+eqnPrefix = template "eqnPrefix"
+
+tblPrefix :: ToMetaValue a => a -> Meta
+tblPrefix = template "tblPrefix"
+
+lstPrefix :: ToMetaValue a => a -> Meta
+lstPrefix = template "lstPrefix"
+
+secPrefix :: ToMetaValue a => a -> Meta
+secPrefix = template "secPrefix"
+
+lofTitle :: ToMetaValue a => a -> Meta
+lofTitle = template "lofTitle"
+
+lotTitle :: ToMetaValue a => a -> Meta
+lotTitle = template "lotTitle"
+
+lolTitle :: ToMetaValue a => a -> Meta
+lolTitle = template "lolTitle"
+
+figureTemplate :: ToMetaValue a => a -> Meta
+figureTemplate = template "figureTemplate"
+
+tableTemplate :: ToMetaValue a => a -> Meta
+tableTemplate = template "tableTemplate"
+
+listingTemplate :: ToMetaValue a => a -> Meta
+listingTemplate = template "listingTemplate"
+
+crossrefYaml :: ToMetaValue a => a -> Meta
+crossrefYaml = template "crossrefYaml"
+
+chaptersDepth :: ToMetaValue a => a -> Meta
+chaptersDepth = template "chaptersDepth"
--- src/Text/Pandoc/CrossRef/Util/Settings/Template.hs
+++ src/Text/Pandoc/CrossRef/Util/Settings/Template.hs
@@ -1,15 +1,8 @@
-{-# LANGUAGE TemplateHaskell, RankNTypes #-}
 module Text.Pandoc.CrossRef.Util.Settings.Template where
-
-import Language.Haskell.TH
-import Language.Haskell.TH.Syntax
 
 import Text.Pandoc.Definition
 import Text.Pandoc.Builder
 import qualified Data.Map as M
 
-genSetting :: String -> Q [Dec]
-genSetting name = sequenceQ
-  [ sigD func [t| forall a. ToMetaValue a => a -> Meta |]
-  , valD (varP func) (normalB [e| Meta . M.singleton $(lift name) . toMetaValue |]) []
-  ] where func = mkName name
+template :: ToMetaValue a => String -> a -> Meta
+template name = Meta . M.singleton name . toMetaValue
