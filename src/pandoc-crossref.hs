cb1dbf4d65ee3351d0752ff48da84bd033d6226f
Nikolay Yakimov <root@livid.pp.ru>
2016-08-10 19:56:49 +0300

    Use defaultCrossRefAction in main exe

--- src/pandoc-crossref.hs
+++ src/pandoc-crossref.hs
@@ -6,9 +6,4 @@ import Text.Pandoc.CrossRef
 main :: IO ()
 main = toJSONFilter go
   where
-    go fmt p@(Pandoc meta _) = runCrossRefIO meta fmt action p
-      where
-        action (Pandoc _ bs) = do
-          meta' <- crossRefMeta
-          bs' <- crossRefBlocks bs
-          return $ Pandoc meta' bs'
+    go fmt p@(Pandoc meta _) = runCrossRefIO meta fmt defaultCrossRefAction p
