5d2a29c37ea9dc3db5d1a4c6b1b57e4934fa2056
Nikolay Yakimov <root@livid.pp.ru>
2017-11-08 00:00:21 +0300

    Flags to open manual; embed manual

--- src/pandoc-crossref.hs
+++ src/pandoc-crossref.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE ApplicativeDo, TemplateHaskell #-}
 import Text.Pandoc
 import Text.Pandoc.Builder
 import Text.Pandoc.JSON
@@ -9,19 +9,36 @@ import qualified Options.Applicative as O
 import Control.Monad
 import Paths_pandoc_crossref (version)
 import Data.Version (showVersion)
+import Data.FileEmbed
+import Web.Browser
+import System.IO.Temp
+import System.IO
+
+man :: String
+man = $(embedStringFile "docs/man.txt")
+
+manHtml :: String
+manHtml = $(embedStringFile "docs/index.html")
+
+data Flag = Version | Man | HtmlMan
 
 run :: Parser (IO ())
 run = do
-  vers <- switch (long "version" <> short 'v' <> help "Print version")
   fmt <- optional $ strOption (
     long "format" <> short 'f' <> metavar "FORMAT" <> help "Force output format"
     )
-  man <- switch (long "man" <> help "Show manpage")
-  return $ go vers man fmt
+  vers <- flag Nothing (Just Version) (long "version" <> short 'v' <> help "Print version")
+  man' <- flag Nothing (Just Man) (long "man" <> help "Show manpage")
+  hman <- flag Nothing (Just HtmlMan) (long "man-html" <> help "Show html manpage")
+  return $ go (vers <|> man' <|> hman) fmt
   where
-    go True _ _ = putStrLn $ showVersion version
-    go _ True _ = putStrLn $ showVersion version
-    go _ _ x = toJSONFilter $ f x
+    go (Just Version) _ = putStrLn $ showVersion version
+    go (Just Man    ) _ = putStrLn man
+    go (Just HtmlMan) _ = withSystemTempFile "pandoc-crossref-manual.html" $ \fp h -> do
+      hPutStrLn h manHtml
+      void $ openBrowser $ "file:///" <> fp
+      return ()
+    go Nothing x = toJSONFilter $ f x
     f ffmt fmt p@(Pandoc meta _) =
       runCrossRefIO meta (Format <$> ffmt <|> fmt) defaultCrossRefAction p
 
