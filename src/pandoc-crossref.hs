eedafb054eeac4c9d5ae64ea21ac761362c74d8a
Nikolay Yakimov <root@livid.pp.ru>
2018-05-02 00:21:24 +0300

    Pandoc 2.2; GHC 8.4

--- src/pandoc-crossref.hs
+++ src/pandoc-crossref.hs
@@ -20,7 +20,6 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 {-# LANGUAGE ApplicativeDo, TemplateHaskell, CPP #-}
 import Text.Pandoc
-import Text.Pandoc.Builder
 import Text.Pandoc.JSON
 
 import Text.Pandoc.CrossRef
