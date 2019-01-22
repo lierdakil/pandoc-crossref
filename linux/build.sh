1f9059ee2070d4ce0cb51ffa2df65ec4f56f5537
Nikolay Yakimov <root@livid.pp.ru>
2019-01-22 22:10:19 +0300

    Travis update: static builds for Linux using docker, upload to bintray

---
+++ linux/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+CMD=\
+"git clone --bare /mnt ./.git
+git config --unset core.bare
+git reset --hard HEAD
+cabal new-build --jobs exe:pandoc-crossref $CABAL_OPTS
+find dist-newstyle -type f -perm +100 -name pandoc-crossref -exec cp {} ./ \;
+#upx --ultra-brute --best pandoc-crossref
+export PANDOC=\"\$(find /root/.cabal -type f -perm +100 -name pandoc -print)\"
+\$PANDOC -s -t man docs/index.md -o pandoc-crossref.1
+PANDOCVER=\$(\$PANDOC --version | head -n1 | cut -f2 -d' ' | tr '.' '_')
+tar czf \"/mnt/linux-pandoc_\$PANDOCVER.tar.gz\" ./pandoc-crossref ./pandoc-crossref.1
+"
+
+docker pull lierdakil/pandoc-crossref-build:latest
+docker run --rm -v "$PWD:/mnt" lierdakil/pandoc-crossref-build:latest /bin/ash -c "$CMD"
