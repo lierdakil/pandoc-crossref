dd61396ca1fe86324b11b475e941de875f37fe7e
Nikolay Yakimov <root@livid.pp.ru>
2019-01-23 19:32:11 +0300

    Try cabal new-build for osx

--- osx/build.sh
+++ osx/build.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-export STACK_YAML=stack-release.yaml
-
-unset CC
-export PATH=/opt/ghc/$GHCVER/bin:$PATH
-mkdir -p ~/.local/bin
-export PATH=$HOME/.local/bin:$PATH
-travis_retry curl -L https://www.stackage.org/stack/osx-x86_64 | tar xz --strip-components=1 -C ~/.local/bin
-stack --no-terminal setup
-stack --no-terminal build
-
-# deployment
-stack --no-terminal install
-stack --no-terminal exec -- pandoc -s -t man docs/index.md -o $HOME/.local/bin/pandoc-crossref.1
-PANDOCVER=$(stack --no-terminal exec -- --version | head -n1 | cut -f2 -d' ' | tr '.' '_')
-RELEASE_FN="macos-pandoc_$PANDOCVER.tar.gz"
-tar czf "$TRAVIS_BUILD_DIR/$RELEASE_FN" -C "$HOME/.local/bin/" "./pandoc-crossref" "./pandoc-crossref.1"
+cabal new-update
+cabal new-build exe:pandoc-crossref $CABAL_OPTS
+find dist-newstyle -type f -perm +100 -name pandoc-crossref -exec cp {} ./ \;
+upx --ultra-brute --best pandoc-crossref
+PANDOC="$(find "$HOME/.cabal" -type f -perm +100 -name pandoc -print)"
+$PANDOC -s -t man docs/index.md -o pandoc-crossref.1
+PANDOCVER=$($PANDOC --version | head -n1 | cut -f2 -d' ' | tr '.' '_')
+tar czf "macos-pandoc_$PANDOCVER.tar.gz" ./pandoc-crossref ./pandoc-crossref.1
