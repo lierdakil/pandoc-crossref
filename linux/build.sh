e679373d33108f008b12f3a4d05e33ed11e7d83a
Nikolay Yakimov <root@livid.pp.ru>
2019-01-23 20:39:06 +0300

    Enable upx packing on Linux

--- linux/build.sh
+++ linux/build.sh
@@ -5,7 +5,7 @@ git config --unset core.bare
 git reset --hard HEAD
 cabal new-build --jobs exe:pandoc-crossref $CABAL_OPTS
 find dist-newstyle -type f -perm +100 -name pandoc-crossref -exec cp {} ./ \;
-#upx --ultra-brute --best pandoc-crossref
+upx --ultra-brute --best pandoc-crossref
 export PANDOC=\"\$(find /root/.cabal -type f -perm +100 -name pandoc -print)\"
 \$PANDOC -s -t man docs/index.md -o pandoc-crossref.1
 PANDOCVER=\$(\$PANDOC --version | head -n1 | cut -f2 -d' ' | tr '.' '_')
