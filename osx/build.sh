715fc28027325bdcdd2d6d07a934bff6e8ba98ff
Nikolay Yakimov <root@livid.pp.ru>
2019-02-15 13:09:45 +0300

    Tweak build scripts

--- osx/build.sh
+++ osx/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cabal new-update
+cabal new-install pandoc $CABAL_OPTS
 cabal new-build exe:pandoc-crossref $CABAL_OPTS
 find dist-newstyle -type f -perm +100 -name pandoc-crossref -exec cp {} ./ \;
 if [ -n "$RUN_UPX" ]; then
   upx --ultra-brute --best pandoc-crossref
 fi
-export PANDOC="$(find "$HOME/.cabal" -type f -perm +100 -name pandoc -print)"
+export PANDOC="$HOME/.cabal/bin/pandoc"
 $PANDOC -s -t man docs/index.md -o pandoc-crossref.1
 PANDOCVER=$($PANDOC --version | head -n1 | cut -f2 -d' ' | tr '.' '_')
 tar czf "macos-pandoc_$PANDOCVER.tar.gz" ./pandoc-crossref ./pandoc-crossref.1
