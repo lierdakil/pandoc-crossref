f506a7dadfcf20dc58cbfc2ad154b73b5c3445e9
Nikolay Yakimov <root@livid.pp.ru>
2019-11-23 13:56:37 +0300

    Don't waste time on ultra-brute upx packaging on macOS

--- osx/build.sh
+++ osx/build.sh
@@ -10,7 +10,7 @@ cabal new-install pandoc $CABAL_OPTS
 cabal new-build exe:pandoc-crossref $CABAL_OPTS
 find dist-newstyle -type f -perm +100 -name pandoc-crossref -exec cp {} ./ \;
 if [ -n "$RUN_UPX" ]; then
-  upx --ultra-brute --best pandoc-crossref
+  upx --best pandoc-crossref
 fi
 $PANDOC -s -t man docs/index.md -o pandoc-crossref.1
 PANDOCVER=$($PANDOC --version | head -n1 | cut -f2 -d' ' | tr '.' '_')
