e65a6cde871d0d5ec40628f7f51c2bdbab7784b7
Nikolay Yakimov <root@livid.pp.ru>
2022-05-21 17:05:13 +0300

    Add gpl header to roman-numerals

--- lib-internal/Text/Numeral/Roman.hs
+++ lib-internal/Text/Numeral/Roman.hs
@@ -1,8 +1,24 @@
 {-
-This file is covered by the license found in licenses/LICENSE.roman-numerals
-
+pandoc-crossref is a pandoc filter for numbering figures,
+equations, tables and cross-references to them.
 Copyright 2009–2014 Roel van Dijk
 Copyright 2022 Nikolay Yakimov <root@livid.pp.ru>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+This file contains code covered by the license found in licenses/LICENSE.roman-numerals
 -}
 
 {-# LANGUAGE FlexibleContexts  #-}
